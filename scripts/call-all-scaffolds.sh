40be0293a03194de6cf8f9d325d6f30673b48f9c
Kevin Lewallen <kevinl@uidaho.edu>
2018-10-26 14:16:14 -0700

    get scaffolds from input

--- scripts/call-all-scaffolds.sh
+++ scripts/call-all-scaffolds.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+#
+# usage: ./thisfile.sh bamfile regionDir
+# regionDir is directory with files named scaf{something} containing a list of regions/scaffolds.
 
 ref="/mnt/lfs2/kevinl/devils/Edited_Dovetail_Genome_12-2017/tasmanian_devil_28Sep2017_aHCF9.fasta"
 samp="$(basename ${1%%.*})"
-cmd1="bcftools mpileup -Ou -f $ref -R "
-cmd2="bcftools call -mv -Ob -o "
-for scaf in scaf-{0..9}
+bcft="~/bin/bcftools"
+cmd1="$bcft mpileup -Ou -f $ref -R"
+cmd2="$bcft call -mv -Ob -o"
+for scaffold in ${2}/scaf*
 do
+	scaf="$(basename $scaffold)"
 	name="${samp}-call-${scaf}"
-	echo $cmd1 $scaf $1 \| $cmd2 ${name} > "run-${name}.sh"
+	out="run-${name}.sh"
+	echo '#!/bin/bash' > $out
+	echo $cmd1 $scaf $1 \| $cmd2 $name >> $out
 done
 
